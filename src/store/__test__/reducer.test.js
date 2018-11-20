e5ab7947cd24993e013dfcab08df0ba1b20a93a7
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2018-11-20 19:26:20 +0300

    add test for DEFAULT_STATE and IMAGE_DOES_NOT_EXIST

--- src/store/__test__/reducer.test.js
+++ src/store/__test__/reducer.test.js
@@ -12,14 +12,48 @@ describe('reducer', () => {
 
     const action = {
       type: 'GET_IMAGE_URL',
-      imageUrl: imageUrl,
-    }
-    
+      imageUrl,
+    };
+
     // Act
     const expectedState = reducer(initialState, action);
 
-    //Arrange
+    // Arrange
     expect(expectedState.resultImage).toEqual(expectedResultImage);
+    expect(expectedState.hasResult).toEqual(expectedHasResult);
+    expect(expectedState.hasCounter).toEqual(expectedHasCounter);
+  });
+
+  test('reducer - IMAGE_DOES_NOT_EXIST', () => {
+    // Assert
+    const timeLeft = 31416222092716;
+    const expectedTimeLeft = timeLeft;
+    const expectedHasResult = true;
+    const expectedHasCounter = true;
+
+    const action = {
+      type: 'IMAGE_DOES_NOT_EXIST',
+      timeLeft,
+    };
+
+    // Act
+    const expectedState = reducer(initialState, action);
+
+    // Arrange
+    expect(expectedState.timeLeft).toEqual(expectedTimeLeft);
+    expect(expectedState.hasResult).toEqual(expectedHasResult);
+    expect(expectedState.hasCounter).toEqual(expectedHasCounter);
+  });
+
+  test('reducer - DEFAULT_STATE', () => {
+    // Assert
+    const expectedHasResult = false;
+    const expectedHasCounter = false;
+
+    // Act
+    const expectedState = reducer(initialState, 'DEFAULT_STATE');
+
+    // Arrange
     expect(expectedState.hasResult).toEqual(expectedHasResult);
     expect(expectedState.hasCounter).toEqual(expectedHasCounter);
   });
